8671341c818b546c509687d0f6388a5aa6c93e28
akhilpandey95 <akhilpandey.akella@gmail.com>
2021-02-25 08:51:37 -0600

    Issue #1: Added feature engineering methods
    - updated src/linguistic_features.py, src/scholarly_features.py and
    src/structural_features.py with methods for collecting the respective
    features.
    
    Signed-off-by: akhilpandey95 <akhilpandey.akella@gmail.com>

--- src/scholarly_features.py
+++ src/scholarly_features.py
@@ -3,3 +3,212 @@
 # file, You can obtain one at
 # https://github.com/reproducibilityproject/featurely/blob/main/LICENSE
 
+import os
+import re
+import sys
+import time
+import random
+import urllib
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from ast import literal_eval
+from bs4 import BeautifulSoup
+from collections import Counter
+
+from selenium import webdriver
+from selenium.webdriver.common.proxy import *
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from openpyxl import Workbook, load_workbook
+
+# function for highlighting the user to solve CAPTCHA
+def solve_captcha():
+    """
+    Post the user a message asking to solve for CAPTCHA
+    Parameters
+    ----------
+    None
+    Returns
+    -------
+    Nothing
+    """
+    raw_input('Please, solve the CAPTCHA and press ENTER: ')
+
+# function for obtaining the altmetric id from the DOI
+def obtain_altmetric_id(dataframe, url):
+    """
+    Obtain the altmetric id by parsing the DOI
+    from the given dataframe
+    Parameters
+    ----------
+    arg1 | dataframe: pandas.DataFrame
+        The pandas dataframe object
+    arg2 | url: str
+        The google scholar URL of the scholarly article
+    Returns
+    -------
+    Integer
+        int
+    """
+    try:
+        # use the url to capture the doi
+        doi = url.split('=')[1]
+
+        # return the altmetric id
+        return dataframe.loc[dataframe.doi.apply(lambda x: x) == doi].altmetric_id.values[0]
+    except:
+        # return an NA value
+        return np.nan
+
+# function for obtaining the citations from the parsed html
+def obtain_citation(file_name):
+    """
+    Obtain the citation from the soup elements
+    Parameters
+    ----------
+    arg1 | file_name: str
+        The name of the file name
+    Returns
+    -------
+    String
+        str
+    """
+    try:
+        # open and read the file
+        f = open(file_name).readlines()
+
+        # read the contents of the file
+        with open(file_name, 'r') as f:
+            # read the soup elements
+            contents = f.read()
+
+            # read the file contents as BS4 object
+            soup = BeautifulSoup(contents, 'lxml')
+
+            # identify all the div elements
+            divs = soup.findAll("div", {"class": "gs_fl"})
+
+            # identify all the hrefs
+            if len(divs) > 1:
+                href = divs[1].find_all('a')[2]
+            else:
+                href = divs[0].find_all('a')[2]
+
+        # return the text
+        return href.text
+    except:
+        # return NA
+        return np.nan
+
+# function for preparing the URL's text file
+def prepare_urls(dataframe, outfile_name, write=False):
+    """
+    Prepare the google scholar urls with DOI's
+    of the scholarly articles added at the end
+    Parameters
+    ----------
+    arg1 | dataframe: pandas.DataFrame
+        The pandas dataframe object
+    arg2 | outfile_name: str
+        The output file name to write the URLS
+    arg3 | write: bool
+        Flag to decide if the file is to be written to disk
+    Returns
+    -------
+    Array
+        list
+    """
+    # read the dataframe
+    dois = dataframe.doi.values.tolist()
+
+    # add the scholarly url
+    scholarly_url = 'https://scholar.google.com/scholar_lookup?doi='
+
+    # add the url to every doi
+    urls = list(map(lambda x: scholarly_url + x, tqdm(dois)))
+
+    # write to a file
+    if write:
+        # write one line at a time
+        for url in tqdm(urls):
+            # open an empty file
+            f = open(outfile_name, 'a')
+
+            # add the urls
+            f.write(url)
+
+            # add a line break
+            f.write('\n')
+
+            # close the file
+            f.close()
+
+    # retun the urls
+    return urls
+
+# function for detecting and solving the captcha
+def checkload(url, element_id):
+    """
+    Function that attempts to solve the
+    given CAPTCHA for the URL
+    Parameters
+    ----------
+    arg1 | url: str
+        The google scholar URL of the scholarly article
+    arg1 | element_id: bs4.
+        The google scholar URL of the scholarly article
+    Returns
+    -------
+    Integer
+        int
+    """
+    # set the flag for checking if the page loaded
+    loaded_correctly = False
+
+    # set the attempt rate to 1
+    attempt = 1
+
+    # until the page has loaded continue
+    while loaded_correctly == False:
+        try:
+            # obtain the url using bs4 object
+            br.get(url)
+
+            # find the element by XPATH
+            html = br.find_element_by_xpath('//html').get_attribute('innerHTML').encode('utf-8')
+
+            # condition 1 for checking the captcha
+            if html.find("Please show you're not a robot") > 0:
+                # post the message
+                solve_captcha()
+            elif html.find("your computer or network may be sending automated queries") > 0:
+                # post the message
+                solve_captcha()
+            elif html.find("Demuestra que no eres un robot") > 0:
+                # post the message
+                solve_captcha()
+            elif html.find("inusual procedente de tu red de ordenadores") > 0:
+                # post the message
+                solve_captcha()
+            elif html.find("Our systems have detected unusual traffic from your computer") > 0:
+                # post the message
+                solve_captcha()
+
+            # identify the correct element ID
+            doc_list = br.find_element_by_class_name(element_id)
+
+            # set the flag to false notifying the end of the loop
+            loaded_correctly = True
+        except:
+            # check if this is the first attempt
+            if attempt == 1:
+                # pipe out message highlighting
+                print('THE FOLLOWING URL COULDN\'T BE DOWNLOADED:')
+                print(url)
+                break
+            loaded_correctly = False
+            print('Trying again ' + url)
+            attempt += 1
+            time.sleep(2)
+
+
--- src/structural_features.py
+++ src/structural_features.py
@@ -3,3 +3,167 @@
 # file, You can obtain one at
 # https://github.com/reproducibilityproject/featurely/blob/main/LICENSE
 
+import os
+import sys, getopt
+from io import StringIO
+from tabula import wrapper
+from PyPDF2 import PdfFileReader
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+
+# function for checking if a pdf has tables or not
+def check_tables(file_name):
+    df = wrapper.read_pdf(str(file_name), pages='all')
+    if len(df) == 0:
+        return False
+    else:
+        return True
+
+# function for checking if equations exist in a file
+def get_references(text):
+    # get the list of refs first
+    list_of_refs = re.findall(r"\[(\d+)\]", text)
+
+    # apply lamba and convert them to numbers
+    list_of_refs = list(map(lambda x: int(x), list_of_refs))
+
+    # return the max of the refs
+    if len(list_of_refs) != 0:
+        if not max(list_of_refs) > 200:
+            return max(list_of_refs)
+        else:
+            return 0
+    else:
+        return 0
+
+# function for getting the conference/journal rank
+def get_conf_journ_rank(name):
+    # add the URL
+    url = 'http://www.conferenceranks.com/?searchall=' + name.split("'")[0] +'#data'
+
+    # call obtain_content and init soup
+    content = obtain_content(url)
+    soup = BS(content, "html.parser")
+
+    # return the rank of the journal/conf
+    return soup.select('div tbody td')[2].get_text()
+
+# function for getting the presence of intro sections
+def is_intro_pres(number):
+    with open('/Users/akhil/Downloads/brown_unv_text/' + number + '.pdf.txt') as f:
+        if 'INTRODUCTION' in f.read() or 'Introduction' in f.read():
+            return 1
+        else:
+            return 0
+
+# function for getting the presence of intro sections
+def is_test_pres(number):
+    with open('/Users/akhil/Downloads/brown_unv_text/' + number + '.pdf.txt') as f:
+        if 'Implementation' in f.read() or 'METHODOLOGY' in f.read():
+            return 1
+        else:
+            return 0
+
+# function for getting the presence of methodology sections
+def is_meth_pres(number):
+    with open('/Users/akhil/Downloads/brown_unv_text/' + number + '.pdf.txt') as f:
+        if 'METHODOLOGY' in f.read() or 'Implementation' in f.read():
+            return 1
+        else:
+            return 0
+
+# function for getting the presence of results sections
+def is_res_pres(number):
+    with open('/Users/akhil/Downloads/brown_unv_text/' + number + '.pdf.txt') as f:
+        if 'RESULTS' in f.read() or 'EVALUATION RESULTS' in f.read():
+            return 1
+        else:
+            return 0
+
+# function for getting the presence of results sections
+def is_alg_pres(number):
+    with open('/Users/akhil/Downloads/brown_unv_text/' + number + '.pdf.txt') as f:
+        if 'Algorithm' in f.read():
+            return 1
+        else:
+            return 0
+
+# function for getting the presence of results sections
+def is_img_pres(number):
+    with open('/Users/akhil/Downloads/brown_unv_text/' + number + '.pdf.txt') as f:
+        if 'Figure' in f.read():
+            return 1
+        else:
+            return 0
+
+# function for getting the presence of results sections
+def is_hyp_pres(number):
+    with open('/Users/akhil/Downloads/brown_unv_text/' + number + '.pdf.txt') as f:
+        if 'http' in f.read() or 'https' in f.read():
+            return 1
+        else:
+            return 0
+
+# function for getting the presence of results sections
+def is_table_pres(number):
+    with open('/Users/akhil/Downloads/brown_unv_text/' + number + '.pdf.txt') as f:
+        if 'Table' in f.read() or 'TABLE' in f.read():
+            return 1
+        else:
+            return 0
+
+# get number of pages
+def no_of_pages(number):
+    # read the pdf file
+    pdf = PdfFileReader(open('/Users/akhil/Downloads/brown_unv/'+ number +'.pdf','rb'))
+
+    # return the number of pages
+    return pdf.getNumPages()
+
+# get number of algorithms present
+def no_alg(full_text):
+    return len(Counter(re.findall('Algorithm\s[0-9]{0,2}', full_text)).keys())
+
+# get number of images present
+def no_images(full_text):
+    return len(Counter(re.findall('Figure\s[0-9]{0,2}', full_text)).keys())
+
+# get number of tables present
+def no_tables(full_text):
+    return len(Counter(re.findall('Table\s[0-9]{0,2}', full_text)).keys())
+
+#converts pdf, returns its text content as a string
+def convert(fname, pages=None):
+    if not pages:
+        pagenums = set()
+    else:
+        pagenums = set(pages)
+
+    output = StringIO()
+    manager = PDFResourceManager()
+    converter = TextConverter(manager, output, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+
+    infile = open(fname, 'rb')
+    for page in PDFPage.get_pages(infile, pagenums):
+        interpreter.process_page(page)
+    infile.close()
+    converter.close()
+    text = output.getvalue()
+    output.close
+    return text
+
+#converts all pdfs in directory to text
+def convert_to_pdf(pdf_dir, txt_dir):
+    if pdf_dir == "": pdf_dir = os.getcwd() + "~/" #if no pdfDir passed in
+    for pdf in tqdm(os.listdir(pdf_dir)): #iterate through pdfs in pdf directory
+        fileExtension = pdf.split(".")[-1]
+        if fileExtension == "pdf":
+            pdf_file_name = pdf_dir + pdf
+            text = convert(pdf_file_name)
+            text_file_name = txt_dir + pdf + ".txt"
+            text_file = open(text_file_name, "w") #make text file
+            text_file.write(text)
+
